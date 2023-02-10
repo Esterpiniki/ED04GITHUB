330419b7f3e0ce233095c89e4457aeeae6e98eb8
Esterfupe <Ester@DESKTOP-A3PVGK7>
2023-02-10 22:02:27 +0100

    Aquí ponmemos los mensajes descriptivos del commit

---
+++ src/ejemplo_Git.java
@@ -0,0 +1,10 @@
+
+public class ejemplo_Git {
+
+	public static void main(String[] args) {
+		System.out.println("Este es el primer commit");
+
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ester
+ *
+ */
+module TareaGitHub {
+}
